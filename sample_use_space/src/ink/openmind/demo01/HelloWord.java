c9b7d06cc2cd386ac68bb55c4ad8ac2f573ea1e2
Sunshineisbright <Wang.zhuang@outlook.com>
2020-11-08 08:45:04 +0800

    Version 1.0.1 - First Update

--- sample_use_space/src/ink/openmind/demo01/HelloWord.java
+++ sample_use_space/src/ink/openmind/demo01/HelloWord.java
@@ -3,11 +3,12 @@ package ink.openmind.demo01;
 /**
  * @author ：Wangzhuang2
  * @version : 1.0.0
- * @date ：Created in 2020/11/8 8:38
+ *
  * DESC
  */
 public class HelloWord {
     public static void main(String[] args) {
+        System.out.println("版本控制系统测试");
         System.out.println("版本控制系统测试");
     }
 }
