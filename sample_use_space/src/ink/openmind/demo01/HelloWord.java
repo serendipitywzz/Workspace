942b88f9ea1a058fe94631bedff73348e8fb8a6a
Wangzz <Wang.zhuang@outlook.com>
2020-11-08 08:53:38 +0800

    Update HelloWord.java

--- sample_use_space/src/ink/openmind/demo01/HelloWord.java
+++ sample_use_space/src/ink/openmind/demo01/HelloWord.java
@@ -10,5 +10,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("版本控制系统测试");
         System.out.println("版本控制系统测试");
+        
+        System.out.println("Hi, 你好这是一个测试，测试如何拉去代码");
     }
 }
